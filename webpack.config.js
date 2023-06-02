f19520ee7821761e2976236a43c4759a80ce4b61
jungle77 <junglej77>
2023-06-02 15:55:12 +0800

    9

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,39 @@
+const fs = require('fs');
 const path = require('path');
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 拷贝静态文件
+// 递归获取目录下所有.js文件作为入口
+function getEntries(dir) {
+    let entries = {};
 
+    function readDir(dir) {
+        const files = fs.readdirSync(dir);
+
+        files.forEach(file => {
+            const filePath = path.resolve(dir, file);
+            const stat = fs.statSync(filePath);
+
+            if (stat.isDirectory()) {
+                readDir(filePath);
+            } else if (stat.isFile() && path.extname(filePath) === '.js') {
+                const relativePath = path.relative('./src', filePath);
+                const entryName = path.dirname(relativePath);
+                entries[entryName] = filePath;
+            }
+        });
+    }
+
+    readDir(dir);
+
+    return entries;
+}
 module.exports = {
     ...defaultConfig,
-    entry: {
-        'block': path.resolve(__dirname, 'src', 'index.js'),
-    },
+    entry: getEntries('./src'),
     output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: '[name]/index.js',
     },
     module: {
         rules: [
@@ -30,12 +54,21 @@ module.exports = {
                     'css-loader',
                     'sass-loader'
                 ]
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
         ]
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: '[name].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/**/block.json', to: 'dist/[path][name][ext]', context: 'src/' },
+            ],
         }),
     ]
 };
