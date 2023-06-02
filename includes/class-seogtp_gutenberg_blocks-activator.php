6a4207c292a4f8e2b75e29beb209a310ba4b41d3
jungle77 <junglej77>
2023-06-02 11:48:21 +0800

    1

---
+++ includes/class-seogtp_gutenberg_blocks-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https:// 
+ * @since      1.0.0
+ *
+ * @package    Seogtp_gutenberg_blocks
+ * @subpackage Seogtp_gutenberg_blocks/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Seogtp_gutenberg_blocks
+ * @subpackage Seogtp_gutenberg_blocks/includes
+ * @author       <447494332@qq.com>
+ */
+class Seogtp_gutenberg_blocks_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
